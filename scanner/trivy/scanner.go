296d233735fa781eb5b1d3fbd8bce28d8173075f
Alex Ashley <alexa@liatrio.com>
2021-09-16 15:14:47 -0400

    Fix ctx cancel error in image scan

--- scanner/trivy/scanner.go
+++ scanner/trivy/scanner.go
@@ -57,7 +57,7 @@ func (t *trivyImageScanner) Init() error {
 	return nil
 }
 
-func (t *trivyImageScanner) ImageScan(imageUri string, ctx context.Context) {
+func (t *trivyImageScanner) ImageScan(imageUri string) {
 	log := t.logger.Named("ImageScan").With(zap.String("imageUri", imageUri))
 	log.Info("Starting scan")
 
@@ -68,6 +68,7 @@ func (t *trivyImageScanner) ImageScan(imageUri string, ctx context.Context) {
 	}
 	log.Debug("Scan completed", zap.Duration("scan", results.ScanEnd.Sub(results.ScanStart)))
 
+	ctx := context.Background()
 	noteName, err := t.createScanNote(ctx, imageUri)
 	if err != nil {
 		log.Error("Error creating scan note", zap.Error(err))
